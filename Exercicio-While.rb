af65b22701bd819a0b5321014f6feda97f0ca674
Lucas Lima <72763196+Delima737@users.noreply.github.com>
2024-02-25 11:44:32 -0300

    update

--- Exercicio-While.rb
+++ Exercicio-While.rb
@@ -1,35 +1,49 @@
 puts "Seja bem vindo prof"
+
+def menu()
+    puts "[1] Cadastrar novo aluno"
+    puts "[2] Ver alunos"
+    puts "[3] Sair"
+    print "Escolha uma opção: "
+    return gets.to_i()
+#não seria necessario usar return pois a ultima linha tem dar return do ultimo comando
+end
+def inserir_alunos()
+    puts 'Digite o nome do aluno: '
+    nome = gets.chomp
+    puts 'Digite o nota do aluno: '
+    nota = gets.chomp
+    puts 'Digite o disciplina do aluno: '
+    disciplina = gets.chomp
+    puts
+    puts "Aluno #{nome} cadastrado com sucesso!"
+    puts
+    return {nome: nome,nota: nota, disciplina: disciplina }
+end
+
+def imprimir_alunos(r)
+    puts
+	puts "================ Alunos cadastrados ============="
+	puts
+    r.each do |alunos|
+        puts"#{alunos[:nome]} , tirou nota: #{alunos[:nota]} na matéria: #{alunos[:disciplina]}"
+    end
+
+end
 alunos = []
-puts "[1] Cadastrar novo aluno"
-puts "[2] Ver alunos"
-puts "[3] Sair"
-print "Escolha uma opção: "
-opcao = gets.to_i()
-puts "==========================="
+opcao = menu()
+
 while (opcao != 3) do
   if (opcao == 1 )
-  puts 'Digite o nome do aluno: '
-  nome_aluno = gets.chomp
-  alunos << nome_aluno
-  puts 'Digite o nota do aluno: '
-    nota_aluno = gets.chomp
-    alunos << nota_aluno
-    puts 'Digite o disciplina do aluno: '
-      disciplina_aluno = gets.chomp
-      alunos << disciplina_aluno
+alunos << inserir_alunos()
 
-    elsif(opcao == 2 )
-        alunos.each do |nome_aluno, nota_aluno, disciplina_aluno|
-    puts nome_aluno.to_s() +"tirou nota ", nota_aluno.to_s(),"na matéria " , disciplina_aluno.to_s()
-    puts alunos
-  end
-else
-  puts 'Opção invalida'
+  elsif(opcao == 2 )
+ imprimir_alunos(alunos)
+  else
+    puts
+    puts 'Opção inválida'
+    puts '=============================================================='
+    puts
 end
-puts "[1] Cadastrar novo aluno"
-puts "[2] Ver alunos"
-puts "[3] Sair"
-print "Escolha uma opção: "
-opcao = gets.to_i()
-puts "==========================="
+    opcao = menu()
 end
--- exercicio-while2.0.rb
+++ exercicio-while2.0.rb
@@ -1,34 +1,44 @@
 puts "Bem-vindo ao seu caderno de receita"
-receitas = []
 
-puts "[1] Cadastrar nova receita"
-puts "[2] Ver receitas"
-puts "[3] Sair"
-print "Escolha uma opção: "
-opcao = gets.to_i()
+def menu ()
+	puts "[1] Cadastrar nova receita"
+	puts "[2] Ver receitas"
+	puts "[3] Sair"
+	print "Escolha uma opção: "
+  return gets.to_i()
+end
+def inserir_receita()
+	puts "Digite o nome da receita: "
+	nome = gets.chomp()
+	puts "Digite o tipo da receita: "
+	tipo = gets.chomp()
+	puts
+	puts "Receita #{nome} cadastrada com sucesso!"
+	puts
+	return  { nome: nome, tipo: tipo }
+end
+def imprimir_receitas(r)
+	puts
+	puts "================ Receitas cadastradas ============="
+	puts
+	r.each do |receita|
+	puts"#{receita[:nome]} - #{receita[:tipo]}"
+end
+end
+receitas = []
+opcao = menu()
+
 while (opcao != 3) do
   if (opcao == 1 )
-		puts "Digite o nome da receita: "
-		nome = gets.chomp()
-
-		receitas << nome
-
-		puts
-		puts "Receita #{nome} cadastrada com sucesso!"
-		puts
-
-
+	receitas << inserir_receita()
 elsif(opcao == 2 )
-	puts "================ Receitas cadastradas ============="
-	puts receitas
-else
-puts 'Opção invalida'
+  imprimir_receitas(receitas)
 
-puts "Obrigado"
+else
+puts
+puts 'Opção inválida'
+puts '=============================================================='
+puts
 end
-puts "[1] Cadastrar nova receita"
-puts "[2] Ver receitas"
-puts "[3] Sair"
-print "Escolha uma opção: "
-opcao = gets.to_i()
+opcao = menu()
 end
